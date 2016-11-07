a32c02bc27ba4f52e2cb92ca486f6b3819919ab7
Peder Dag <Peder Dag>
2016-11-07 14:40:19 +0100

    Finere kode paa magic header

--- studio-2016/scripts/animated_header.js
+++ studio-2016/scripts/animated_header.js
@@ -1,14 +1,8 @@
 window.onscroll = function(event) {
     var yPos = pageYOffset;
+    var sweetSpot = 12.6;
     var header = document.getElementById("headerID");
-    console.log(header); //14
-    if(yPos < 12.6){
-        header.style.visibility = "visible";
-        header.style.opacity = 1-(yPos/12.6);
-        console.log("Setting visibility to true")
-    }
-    else{
-        header.style.visibility = "hidden"; 
-        console.log("Setting visibility to false");
-    }
+
+    header.style.opacity = 1-(yPos-sweetSpot)/sweetSpot;
+
 }
