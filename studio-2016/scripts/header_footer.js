e89b9e93dacca797437a62c354390eef0ad6b191
torgerz <sebastwt@stud.ntnu.no>
2016-11-07 16:20:33 +0100

    daf

--- studio-2016/scripts/header_footer.js
+++ studio-2016/scripts/header_footer.js
@@ -28,22 +28,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		elements.header.appendChild(h_div);	
 	}
 	if (elements.navbar) {
-		//var ddl_js = document.createElement("script");
-		//ddl_js.className = "dropdown";
-		//ddl_js.type = "text/javascript";
-		//ddl_js.src = "..dropdown-studio.js";
-		//elements.navbar.appendChild(ddl_js);
-
 		var n_div = document.createElement("div");
 		n_div.className = "nav-bar";
-		n_div.innerHTML = "<a href=\"index/information.html\" class=\"tab\">Information</a><div class=\"dropdown\"><button onclick=\"myFunction()\" class=\"dropbtn\">Studio</button><div id=\"dropdown\" class=\"dropdown-content\"><a href=\"studio.html\">Nei</a><a href=\"studio/equipment.html\">Equipment</a></div></div><a href=\"who-are-we.html\" class=\"tab\">Who are we</a><a href=\"index.html\" class=\"tab img\"></a><a href=\"producers.html\" class=\"tab\">Producers</a><a href=\"lessons.html\" class=\"tab\">Lessons</a><a href=\"prices.html\" class=\"tab\">Price</a>";
+		n_div.innerHTML = "<a href=\"index/information.html\" class=\"tab\">Information</a><div class=\"dropdown\"><button onclick=\"myFunction()\" class=\"dropbtn\">Studio</button><div id=\"dropdown\" class=\"dropdown-content\"><a href=\"studio.html\">Studio</a><a href=\"studio/equipment.html\">Equipment</a></div></div><a href=\"who-are-we.html\" class=\"tab\">Who are we</a><a href=\"index.html\" class=\"tab img\"></a><div class=\"dropdown\"><button onclick=\"myFunction()\" class=\"dropbtn\"><a href=\"producers.html\">Producers</a></button><div id=\"dropdown\" class=\"dropdown-content\"><a href=\"producers/PerKristian.html\">Per Kristian</a><a href=\"producers/Espen.html\">Espen</a><a href=\"producers/Steffen.html\">Steffen</a></div></div><a href=\"lessons.html\" class=\"tab\">Lessons</a><a href=\"prices.html\" class=\"tab\">Price</a>";
 		elements.navbar.appendChild(n_div);
 	}
 	if (elements.footer) {
 		var f_div = document.createElement("div");
 		f_div.className = "footer";
-		//Lag link til contact us
-		f_div.innerHTML = "<p><a class=\"header-link\" href=\"mailto:northernwave@gmail.com\">Contact us<a> | Northern Wave</p>";
+		f_div.innerHTML = "<p><a href=\"index/information.html\">Contact us<a> | Northern Wave</p>";
 		elements.footer.appendChild(f_div);	
 	}
 	if (elementsfolder.headerf) {
@@ -57,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		var n_div = document.createElement("div");
 		n_div.className = "nav-bar";
 
-		n_div.innerHTML = "<a href=\"../index/information.html\" class=\"tab\">Information</a><div class=\"dropdown\"><button onclick=\"myFunction()\" class=\"dropbtn\">Studio</button><div id=\"dropdown\" class=\"dropdown-content\"><a href=\"../studio.html\">Studio</a><a href=\"../studio/equipment.html\">Equipment</a></div></div><a href=\"../who-are-we.html\" class=\"tab\">Who are we</a><a href=\"../index.html\" class=\"tab img\"></a><a href=\"../producers.html\" class=\"tab\">Producers</a><a href=\"../lessons.html\" class=\"tab\">Lessons</a><a href=\"../prices.html\" class=\"tab\">Price</a>";
+		n_div.innerHTML = "<a href=\"../index/information.html\" class=\"tab\">Information</a><div class=\"dropdown\"><button onclick=\"myFunction()\" class=\"dropbtn\">Studio</button><div id=\"dropdown\" class=\"dropdown-content\"><a href=\"../studio.html\">Studio</a><a href=\"../studio/equipment.html\">Equipment</a></div></div><a href=\"../who-are-we.html\" class=\"tab\">Who are we</a><a href=\"../index.html\" class=\"tab img\"></a><div class=\"dropdown\"><button onclick=\"myFunction()\" class=\"dropbtn\"><a href=\"../producers.html\">Producers</a></button><div id=\"dropdown\" class=\"dropdown-content\"><a href=\"../producers/PerKristian.html\">Per Kristian</a><a href=\"../producers/Espen.html\">Espen</a><a href=\"../producers/Steffen.html\">Steffen</a></div></div><a href=\"../lessons.html\" class=\"tab\">Lessons</a><a href=\"../prices.html\" class=\"tab\">Price</a>";
 		elementsfolder.navbarf.appendChild(n_div);	
 	}
 	if (elementsfolder.footerf) {
