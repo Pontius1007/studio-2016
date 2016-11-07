161cb65d61d73a06313d6e9e4f3f4635ee02e265
Peder Dag <Peder Dag>
2016-11-07 14:55:41 +0100

    Kommentert i anim_header og litt mer i P4

--- studio-2016/scripts/animated_header.js
+++ studio-2016/scripts/animated_header.js
@@ -1,8 +1,16 @@
-window.onscroll = function(event) {
-    var yPos = pageYOffset;
-    var sweetSpot = 12.6;
-    var header = document.getElementById("headerID");
+/*
+FILE NAME: scripts/animated_header.js
+WRITTEN BY: Anders Brevik
+WHEN: November 2016
+PURPOSE: To create a fade effect on the header when the user scrolls.
+*/
 
-    header.style.opacity = 1-(yPos-sweetSpot)/sweetSpot;
+window.onscroll = function(event) { // Runs when the user scrolls.
+    var yPos = pageYOffset;  // Stores the current yPosition on the page
+    var sweetSpot = 12.6;   // A yOffsett that looks and feels right
+    var header = document.getElementById("headerID"); // Gets the correct header element.
+
+    header.style.opacity = 1-(yPos-sweetSpot)/sweetSpot; // Starts lowering the opacity when the yPosition is at the sweetSpot, is fully
+                                                        // opaque when y is at 2 * sweetSpot
 
 }
