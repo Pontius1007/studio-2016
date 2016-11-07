1927a90a8ed69dcadf93038f130063526340cb7a
torgerz <sebastwt@stud.ntnu.no>
2016-11-07 13:22:30 +0100

    Merge branch 'master' of https://github.com/Pontius1007/studio-2016
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- studio-2016/scripts/formValidation.js
+++ studio-2016/scripts/formValidation.js
@@ -1,7 +1,17 @@
+/*
+FILE NAME: formValidation.js
+WRITTEN BY: Embrik Tvenge Einang
+WHEN: November 2016
+PURPOSE: Checks that the user has correctly entered both mail and Name.
+Will only check if the name field is not empty due to the difficulty with checking
+regional and strange names. 
+*/
+
 function formValidation() {
 	var navn = document.forms["producerForm"]["name"].value;
 	var mail = document.forms["producerForm"]["email"].value;
 	var regex = /\S+@\S+\.\S+/;
+
 
 	if(navn == null || navn == "") {
 		alert("Please enter a name!");
@@ -16,5 +26,6 @@ function formValidation() {
 	else {
 		return true;
 	}
-
 }
+
+
